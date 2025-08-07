75169c72ea9e05ebc869131f98c6be53dd78f5e0
VanshGosavi07 <vanshgosavi7@gmail.com>
2025-08-07 20:03:26 +0530

    deployemnt settins added

--- parksons_graphics_task/urls.py
+++ parksons_graphics_task/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("home.urls")),  # Web interface
     path("api/", include("home.api_urls")),  # API endpoints
+    # Redirect /accounts/login/ to /login/
+    path("accounts/login/", RedirectView.as_view(url="/login/", permanent=True)),
 ]
--- parksons_graphics_task/wsgi.py
+++ parksons_graphics_task/wsgi.py
@@ -13,6 +13,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "parksons_graphics_task.settings")
 
 application = get_wsgi_application()
-
-# Vercel serverless function
-app = application
